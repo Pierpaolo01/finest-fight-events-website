ff1724a37d6972beb6c39f09ee772c287752b1dd
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2023-11-17 21:35:30 +0100

    Responsive navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <nav :class="{ 'navbar--sticky': isSticky }" class="navbar">
-    <Container class="navbar__content mx-auto px-4 rounded-md">
+    <Container class="navbar__content-tablet">
       <div class="space-x-4">
         <a
           href="http://"
@@ -39,6 +39,47 @@
         <Button type="primary" text="tickets" />
       </div>
     </Container>
+    <Container class="navbar__content-mobile">
+      <div class="self-center">
+        <img
+          src="/fdg-logo.png"
+          alt="fight & dine company logo"
+          class="h-[38px] tablet:h-[60px] tablet:w-[108px]"
+        />
+      </div>
+
+      <IconHamburger @click="mobileNavIsOpen = !mobileNavIsOpen" />
+    </Container>
+    <Container
+      class="space-y-4 flex flex-col bg-white pb-4"
+      v-if="mobileNavIsOpen"
+    >
+      <a
+        href="http://"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="link hover:bg-ffe-bg/10 p-4 -mx-4"
+      >
+        online store
+      </a>
+      <a
+        href="http://"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="link hover:bg-ffe-bg/10 p-4 -mx-4"
+      >
+        events
+      </a>
+      <a
+        href="http://"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="link hover:bg-ffe-bg/10 p-4 -mx-4"
+      >
+        line up
+      </a>
+      <Button type="primary" text="tickets" />
+    </Container>
   </nav>
 </template>
 
@@ -46,6 +87,7 @@
 import { onMounted, onUnmounted, ref } from "vue";
 
 const isSticky = ref(false);
+const mobileNavIsOpen = ref(false);
 
 function handleScroll() {
   isSticky.value = window.scrollY > 64;
@@ -62,15 +104,35 @@ onUnmounted(() => {
 
 <style scoped>
 .navbar {
-  @apply absolute top-16 mx-auto w-full z-10 transition-colors duration-200;
+  @apply absolute top-0 w-full z-50 transition-colors duration-200;
+
+  @screen laptop {
+    @apply top-16 mx-auto px-8;
+  }
 }
 
 .navbar--sticky {
   @apply fixed top-0 w-full bg-white;
 }
 
-.navbar__content {
-  @apply flex w-full bg-white/80 justify-between items-center h-[68px];
+.navbar__content-tablet {
+  @apply hidden;
+
+  @screen tablet {
+    @apply flex w-full bg-white justify-between items-center h-[68px] mx-auto px-4;
+  }
+
+  @screen laptop {
+    @apply rounded-md bg-white/80;
+  }
+}
+
+.navbar__content-mobile {
+  @apply flex w-full bg-white justify-between items-center h-[68px] mx-auto px-4;
+
+  @screen tablet {
+    @apply hidden;
+  }
 }
 
 .navbar__item {
