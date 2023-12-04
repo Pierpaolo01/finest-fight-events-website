fcfafa2a2936f65b62cde21574a19cf280d3142a
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2023-12-04 21:53:57 +0100

    Basic SEO

--- app.vue
+++ app.vue
@@ -4,6 +4,36 @@
   <Footer />
 </template>
 
+<script setup lang="ts">
+useSeoMeta({
+  description:
+    "Bij Finest Fight Events brengen we het beste van twee werelden samen: de opwindende wereld van vechtsport en de verfijnde kunst van het dineren.",
+  ogTitle: "Finest Fight Events",
+  ogDescription:
+    "Bij Finest Fight Events brengen we het beste van twee werelden samen: de opwindende wereld van vechtsport en de verfijnde kunst van het dineren",
+  ogImage: "/FFE_Logo-light.png",
+  ogUrl: "https://finestfightevents.nl/",
+  twitterTitle: "Finest Fight Events",
+  twitterDescription:
+    "Bij Finest Fight Events brengen we het beste van twee werelden samen: de opwindende wereld van vechtsport en de verfijnde kunst van het dineren.",
+  twitterImage: "/FFE_Logo-light.png",
+  twitterCard: "summary",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
+</script>
+
 <style>
 .link {
   @apply text-ffe-bg uppercase tracking-widest font-bold text-base;
